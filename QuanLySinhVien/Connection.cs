896bc60b7497df895a2f39a733a4ca4dc88b5c14
mnduc9802 <mnduc9802@gmail.com>
2024-04-16 21:28:17 +0700

    Add project files.

---
+++ QuanLySinhVien/Connection.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace QuanLySinhVien
+{
+    class Connection
+    {
+        private static string strConnect = @"Data Source=DESKTOP-82JIA37\SQLEXPRESS;Initial Catalog = QuanLySinhVien; Integrated Security = True";
+        public static SqlConnection GetConnection()
+        {
+            SqlConnection conn = new SqlConnection(strConnect);
+            return conn;
+        }
+    }
+}
---
+++ QuanLySinhVien/QuanLySinhVien.Designer.cs
@@ -0,0 +1,204 @@
+﻿namespace QuanLySinhVien
+{
+    partial class QuanLySinhVien
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.btn_dangnhap = new System.Windows.Forms.Button();
+            this.btn_dangky = new System.Windows.Forms.Button();
+            this.btn_tt = new System.Windows.Forms.Button();
+            this.btn_thoat = new System.Windows.Forms.Button();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.lb_truong = new System.Windows.Forms.Label();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            this.groupBox2.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // btn_dangnhap
+            // 
+            this.btn_dangnhap.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.btn_dangnhap.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn_dangnhap.ForeColor = System.Drawing.SystemColors.ControlText;
+            this.btn_dangnhap.Location = new System.Drawing.Point(89, 283);
+            this.btn_dangnhap.Name = "btn_dangnhap";
+            this.btn_dangnhap.Size = new System.Drawing.Size(158, 47);
+            this.btn_dangnhap.TabIndex = 2;
+            this.btn_dangnhap.Text = "Đăng Nhập";
+            this.btn_dangnhap.UseVisualStyleBackColor = true;
+            this.btn_dangnhap.Click += new System.EventHandler(this.btn_dangnhap_Click);
+            // 
+            // btn_dangky
+            // 
+            this.btn_dangky.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.btn_dangky.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn_dangky.ForeColor = System.Drawing.SystemColors.ControlText;
+            this.btn_dangky.Location = new System.Drawing.Point(89, 418);
+            this.btn_dangky.Name = "btn_dangky";
+            this.btn_dangky.Size = new System.Drawing.Size(158, 47);
+            this.btn_dangky.TabIndex = 3;
+            this.btn_dangky.Text = "Đăng Ký";
+            this.btn_dangky.UseVisualStyleBackColor = true;
+            this.btn_dangky.Click += new System.EventHandler(this.btn_dangky_Click);
+            // 
+            // btn_tt
+            // 
+            this.btn_tt.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.btn_tt.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn_tt.ForeColor = System.Drawing.SystemColors.ControlText;
+            this.btn_tt.Location = new System.Drawing.Point(758, 283);
+            this.btn_tt.Name = "btn_tt";
+            this.btn_tt.Size = new System.Drawing.Size(156, 47);
+            this.btn_tt.TabIndex = 4;
+            this.btn_tt.Text = "Thông Tin";
+            this.btn_tt.UseVisualStyleBackColor = true;
+            this.btn_tt.Click += new System.EventHandler(this.btn_tt_Click);
+            // 
+            // btn_thoat
+            // 
+            this.btn_thoat.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.btn_thoat.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn_thoat.ForeColor = System.Drawing.SystemColors.Desktop;
+            this.btn_thoat.Location = new System.Drawing.Point(758, 418);
+            this.btn_thoat.Name = "btn_thoat";
+            this.btn_thoat.Size = new System.Drawing.Size(156, 47);
+            this.btn_thoat.TabIndex = 5;
+            this.btn_thoat.Text = "Thoát";
+            this.btn_thoat.UseVisualStyleBackColor = true;
+            this.btn_thoat.Click += new System.EventHandler(this.btn_thoat_Click);
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.groupBox2.BackColor = System.Drawing.SystemColors.Control;
+            this.groupBox2.Controls.Add(this.lb_truong);
+            this.groupBox2.Location = new System.Drawing.Point(276, 365);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(444, 100);
+            this.groupBox2.TabIndex = 6;
+            this.groupBox2.TabStop = false;
+            // 
+            // lb_truong
+            // 
+            this.lb_truong.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.lb_truong.AutoSize = true;
+            this.lb_truong.Font = new System.Drawing.Font("Microsoft Sans Serif", 16.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lb_truong.ForeColor = System.Drawing.SystemColors.ControlText;
+            this.lb_truong.Location = new System.Drawing.Point(6, 38);
+            this.lb_truong.Name = "lb_truong";
+            this.lb_truong.Size = new System.Drawing.Size(652, 32);
+            this.lb_truong.TabIndex = 0;
+            this.lb_truong.Text = "Trường Đại Học Kinh Tế Kỹ Thuật Công Nghiệp";
+            // 
+            // timer1
+            // 
+            this.timer1.Enabled = true;
+            this.timer1.Interval = 50;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.pictureBox1.Image = global::QuanLySinhVien.Properties.Resources.user_icons;
+            this.pictureBox1.Location = new System.Drawing.Point(403, 218);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(175, 141);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 7;
+            this.pictureBox1.TabStop = false;
+            // 
+            // label1
+            // 
+            this.label1.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 48F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.SystemColors.ControlText;
+            this.label1.Location = new System.Drawing.Point(145, 88);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(713, 91);
+            this.label1.TabIndex = 8;
+            this.label1.Text = "Quản Lý Sinh Viên";
+            this.label1.Click += new System.EventHandler(this.label1_Click_1);
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.pictureBox2.BackgroundImage = global::QuanLySinhVien.Properties.Resources._1200px_UNETI;
+            this.pictureBox2.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
+            this.pictureBox2.Location = new System.Drawing.Point(825, -2);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(178, 150);
+            this.pictureBox2.TabIndex = 9;
+            this.pictureBox2.TabStop = false;
+            // 
+            // QuanLySinhVien
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.BackgroundImage = global::QuanLySinhVien.Properties.Resources._136400048_3617316595026404_8312150713167442017_n;
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.ClientSize = new System.Drawing.Size(1002, 548);
+            this.Controls.Add(this.pictureBox2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.groupBox2);
+            this.Controls.Add(this.btn_thoat);
+            this.Controls.Add(this.btn_tt);
+            this.Controls.Add(this.btn_dangky);
+            this.Controls.Add(this.btn_dangnhap);
+            this.Name = "QuanLySinhVien";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Quản Lý Sinh Viên";
+            this.WindowState = System.Windows.Forms.FormWindowState.Maximized;
+            this.groupBox2.ResumeLayout(false);
+            this.groupBox2.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Button btn_dangnhap;
+        private System.Windows.Forms.Button btn_dangky;
+        private System.Windows.Forms.Button btn_tt;
+        private System.Windows.Forms.Button btn_thoat;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.Label lb_truong;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.PictureBox pictureBox2;
+    }
+}
